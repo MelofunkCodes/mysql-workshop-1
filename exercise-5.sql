4208ea8f0db439575e5a21cf7ad2a72600e47bfa
MelofunkCodes <myeoh27@gmail.com>
2017-01-23 19:32:13 +0000

    Added exercises 5, 6, and 7

---
+++ exercise-5.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Account (id int auto_increment primary key); --creates table with one column named "id", taking integer values, auto incrementing
+
+CREATE TABLE AdressBook (id int auto_increment primary key); 
+CREATE TABLE Entry (id int auto_increment primary key); 
+CREATE TABLE Test (id int auto_increment primary key); 
